c49cf50491c4cf77ca93466e40e78b94a0800420
Adarsh <jha01adarsh@gmail.com>
2023-12-18 13:53:52 +0530

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3); //not aval. in nodejs.avl in browser
+
+let name = "John"; //strin DT
+let age =15; // Number DT
+let isLoggedIn = true; //boolean DT
+
+/**
+ * number
+ * string
+ * bigint
+ * boolean
+ * null -> standalone value
+ * undefined
+ * symbol -> for unique, react 
+ * object
+ */
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof symbol); // undefined
+console.log(typeof null); // object
