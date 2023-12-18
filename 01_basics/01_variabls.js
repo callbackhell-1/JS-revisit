9c15c313235fa53a189ed8ef4e6e4420721105ca
Adarsh <jha01adarsh@gmail.com>
2023-12-18 13:08:27 +0530

    JS variable

--- 01_basics/01_variabls.js
+++ 01_basics/01_variabls.js
@@ -1,1 +1,20 @@
 console.log("Hello World");
+
+const accountId = 15;
+let accountEmail = "hello@email.com";
+var accountPass = "12Asdf58@";
+accountCity = "LA";
+let accountState;
+
+// accountId = 18; // error :TypeError: Assignment to constant variable.
+accountEmail = "email@email.com";
+accountPass = "12345";
+accountCity = "Bengaluru";
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPass,
+  accountCity,
+  accountState,
+]);
