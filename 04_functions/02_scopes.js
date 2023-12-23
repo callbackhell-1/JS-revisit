828a7abfda9d96e50cba808a527e62d18646e339
Adarsh <jha01adarsh@gmail.com>
2023-12-23 23:58:54 +0530

    scopes,closure,function expression,hoisting

--- 04_functions/02_scopes.js
+++ 04_functions/02_scopes.js
@@ -27,3 +27,50 @@ var y = 600;
 
 console.log(x); //500
 console.log(y); //9
+
+/************************/
+function one() {
+  const username = "John";
+
+  function two() {
+    const website = "youtube";
+    console.log(username); //John
+  }
+  console.log(website); //ReferenceError: website is not defined
+
+  two();
+}
+
+one();
+
+/**
+ * CLOSURE -> child function is able to access parent function variables.
+ */
+
+/*************function expression */
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(5, 6)); //11
+
+//this is func expression
+const mul = function (num1, num2) {
+  return num1 * num2;
+};
+
+console.log(mul(5, 6)); //30
+
+/**hoisting */
+console.log(add(5, 6)); //11 (no error in this case)
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(mul1(5, 6)); // error : Cannot access 'mul1' before initialization (no hoisting in this case)
+
+const mul1 = function (num1, num2) {
+  return num1 * num2;
+};
