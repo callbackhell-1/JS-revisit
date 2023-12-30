523d72dcd3d25ddd05f854d3c2846fbdc0252c77
Adarsh <jha01adarsh@gmail.com>
2023-12-31 04:07:51 +0530

    bg-color-changer in optimised way & corner cases

--- 10_async/projects/01_random_color_generator/index.js
+++ 10_async/projects/01_random_color_generator/index.js
@@ -10,7 +10,10 @@ function randomColor() {
 }
 let intervalId;
 function startChangingColor() {
-  intervalId = setInterval(changeBgcolor, 1000);
+  if (!intervalId) {
+    //(intervalId == null)
+    intervalId = setInterval(changeBgcolor, 1000);
+  }
 
   function changeBgcolor() {
     document.body.style.backgroundColor = randomColor();
@@ -18,12 +21,21 @@ function startChangingColor() {
 }
 function stopChangingColor() {
   clearInterval(intervalId);
+  intervalId = null;
 }
 
 /**
- * Problem with this code is, once we start and clicked multiple times on start it , does't stop even if we click on stop button
+ * Problem : Problem with this code is, once we start and clicked multiple times on start it , does't stop even if we click on stop button
+ *
+ * Reason : when we clicked on start button multiple times. Then we overwrite the intervalId value, so when we click on stop button, it stop the latest setInterval , bcoz only the latest setInterval id is there.
+ *
+ * Solution : Once we click on stop button and pass the intervalId to clearInterval, make the intervalId as null. but why ? so what happen if we click again on start button.
+ *
+ * It will got to startChangingColor function and check whether  intervalId is nll or not,
+ * if null then start the interval , if not nothing happen.
+ * if interval starts : we will get a "BRAND NEW intervalId".
+ * Now when we tries to stop the interval, we [ass the latest intervalId.
  */
 
-document.getElementById("start").addEventListener("click", startChangingColor); 
+document.getElementById("start").addEventListener("click", startChangingColor);
 document.getElementById("stop").addEventListener("click", stopChangingColor);
-
