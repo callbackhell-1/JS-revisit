b7b64ce628c0fc204e0fa1165f6662f9bfc6e207
Adarsh <jha01adarsh@gmail.com>
2024-01-03 03:42:28 +0530

    added prototype method in function

--- 12_oop/03_prototype/01_oop.js
+++ 12_oop/03_prototype/01_oop.js
@@ -5,3 +5,25 @@ function multiplyBy5(num) {
 console.log(multiplyBy5(5));
 console.log(multiplyBy5.power);
 console.log(multiplyBy5.prototype);
+
+//creating a prototype method
+function createUser(username, score) {
+  this.username = username;
+  this.score = score;
+}
+
+createUser.prototype.increment = function () {
+  this.score++;
+  console.log(this.score);
+};
+
+createUser.prototype.printMe = function () {
+  console.log(`price is ${this.score}`);
+};
+
+// const user1= createUser("john",10);
+// user1.printMe(); // error: Cannot read properties of undefined (user1.printMe();)
+
+const user1= new createUser("john",10);
+user1.printMe();//price is 10
+user1.increment(); //11
