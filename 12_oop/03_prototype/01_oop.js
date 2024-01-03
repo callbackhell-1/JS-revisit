df8f5311b136084fa03bb0e7708bee927f8091d3
Adarsh <jha01adarsh@gmail.com>
2024-01-03 16:44:38 +0530

    new keyword in js object

--- 12_oop/03_prototype/01_oop.js
+++ 12_oop/03_prototype/01_oop.js
@@ -24,6 +24,23 @@ createUser.prototype.printMe = function () {
 // const user1= createUser("john",10);
 // user1.printMe(); // error: Cannot read properties of undefined (user1.printMe();)
 
-const user1= new createUser("john",10);
-user1.printMe();//price is 10
+const user1 = new createUser("john", 10);
+user1.printMe(); //price is 10
 user1.increment(); //11
+
+/**
+ new keyword:
+
+ Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+JS gives constructor function not from class , but from new .
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+ */
