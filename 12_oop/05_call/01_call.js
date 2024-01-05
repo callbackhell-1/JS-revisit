a62433df4a8d01117d66088b20564cbb86eb2b95
Adarsh <jha01adarsh@gmail.com>
2024-01-05 23:34:54 +0530

    with  call

--- 12_oop/05_call/01_call.js
+++ 12_oop/05_call/01_call.js
@@ -17,3 +17,28 @@ console.log(john);
  createUser { email: 'john@email.com', password: '122' }
 } 
  */
+function SetUsername(username) {
+  //complex DB calls
+  this.username = username;
+}
+
+function createUser(username, email, password) {
+  SetUsername.call(this, username);
+
+  this.email = email;
+  this.password = password;
+}
+
+const john1 = new createUser("johndoe", "john@email.com", "122");
+
+console.log(john1);
+
+/**
+ * createUser {
+  username: 'johndoe',
+  email: 'john@email.com',
+  password: '122'
+}
+--> call pass our execution context to other funtion.
+
+ */
