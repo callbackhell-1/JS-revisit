9a0e48ca3d098eb932b93000f5dccc2fe2e54eac
Adarsh <jha01adarsh@gmail.com>
2024-01-08 19:40:17 +0530

    class - behind the scene

--- 12_oop/06_class/01_class.js
+++ 12_oop/06_class/01_class.js
@@ -26,3 +26,32 @@ console.log(john.changeUsername()); //JOHN
  * when constructor called ?
  * when an object is initilised using class( using new keyword), constructor will call automatically.
  */
+
+/***********************BEHIND THE SCENE****************/
+
+//What if we didn't get the class then how we do the same thing
+
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+User.prototype.encryptPassword = function () {
+  return `${this.password}abc`;
+};
+User.prototype.changeUsername = function () {
+  return `${this.username.toUpperCase()}`;
+};
+
+const wick = new User("wicky", "wick@email.com", 1234);
+console.log(wick);
+
+console.log(wick.changeUsername());
+console.log(wick.encryptPassword());
+
+/**
+ * User { username: 'wicky', email: 'wick@email.com', password: 1234 }
+WICKY
+1234abc
+ */
