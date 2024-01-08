27c65741ce1e034f49a4abcc93223f395f4eebce
Adarsh <jha01adarsh@gmail.com>
2024-01-08 19:50:45 +0530

    super keyword

--- 12_oop/06_class/inheritance.js
+++ 12_oop/06_class/inheritance.js
@@ -10,5 +10,16 @@ class User {
 
 //when we built any ecom website/hospital/edtech app then sometimes we need to make this user admin/doctor/teacher/student .
 
-class Teacher extends User {}
-//By this we get all the property of User into Teacher class
+class Teacher extends User {
+  constructor(username, email, password) {
+    super(username);
+    this.email = email;
+    this.password = password;
+  }
+}
+
+/**
+ * here super keyword refer, from which class we extends.
+ * so here in our case it goes to User class , then go to constructor class and automatically "this"  also sent from Teacher class and that username will be set in "this".
+ * and we get username from User class to Teacher class
+ */
